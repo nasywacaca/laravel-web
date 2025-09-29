e5f04da66f1b535eacf1fbd4cec1dbd85e32230e
nasywacaca <nasywa24ti@mahasiswa.pcr.ac.id>
2025-09-29 14:00:09 +0700

    commit

---
+++ app/Http/Controllers/MataKuliahController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatakuliahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return "menampilkan data matakuliah" ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $kode = null)
+    {
+        if($kode == 'ST445'){
+            return view('Matakuliah-ST445');
+        } else {
+            return view('Matakuliah-noParameter');
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/Matakuliah-ST445.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+  <h1>Selamat Datang!</h1>
+  <h2>anda mengakses matakuliah ST445</h2>
+</body>
+</html>
